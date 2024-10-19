1af53029188aaffa9d796589ad9e9178f5e7b0b8
Vishwa <imt_2022119@iiitm.ac.in>
2024-10-20 03:17:52 +0530

    CORS

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = require("socket.io")(3001, {
   cors: {
-    origin: "*",
+    origin: "https://chess-mate-one.vercel.app/",
   },
 });
 
